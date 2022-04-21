baa1d2e1982d35032249491b62b24e35577255f7
孙笑川 <211837568@qq.com>
2022-04-21 16:24:39 +0800

    合并分支第一次 commit

--- src/main/java/com/git/test/Test.java
+++ src/main/java/com/git/test/Test.java
@@ -8,5 +8,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("你好世界");
         System.out.println("你好世界芜湖起飞");
+        System.out.println("hot-fix分支");
     }
 }
